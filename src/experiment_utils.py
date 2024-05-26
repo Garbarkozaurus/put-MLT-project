fa24e344f63014fa2af8b0948541ba770a7a9611
Garbarkozaurus <marcin.golski@student.put.poznan.pl>
2024-05-26 15:27:33 +0200

    Performing basic regularization experiments

---
+++ src/experiment_utils.py
@@ -0,0 +1,66 @@
+from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+import data_loading
+from sklearn.metrics import accuracy_score
+import numpy as np
+from sklearn.model_selection import train_test_split
+from datetime import datetime
+
+from typing import Literal
+
+
+def regularization_experiment(
+    dataset: Literal["penguins"] | Literal["wine"],
+    model_type: Literal["linear"] | Literal["rbf"],
+    regularization_coefficients: np.ndarray[np.float32],
+    data_split_seed: int = 0,
+    verbose: bool = False,
+) -> np.ndarray[np.float32]:
+    match dataset:
+        case "penguins":
+            df = data_loading.load_penguins()
+            train_df, test_df = train_test_split(
+                df, stratify=df["species"], random_state=data_split_seed
+            )
+        case "wine":
+            df = data_loading.load_wine()
+            train_df, test_df = train_test_split(
+                df, stratify=df["quality"], random_state=data_split_seed
+            )
+        case _:
+            raise ValueError(
+                f"Incorrect dataset name: '{dataset}'. Expected one of: 'penguins', 'wine'"
+            )
+    x_train = train_df.iloc[:, :-1]
+    y_train = train_df.iloc[:, -1]
+    x_test = test_df.iloc[:, :-1]
+    y_test = test_df.iloc[:, -1]
+    accuracies = np.zeros((len(regularization_coefficients), 2))
+    for i, r_coef in enumerate(regularization_coefficients):
+        match model_type:
+            case "linear":
+                model = LinearSVC(C=r_coef, max_iter=100000)
+            case "rbf":
+                model = SVC(C=r_coef)
+            case _:
+                raise ValueError(
+                    f"Incorrect model type: '{model_type}'. Expected one of: 'linear'"
+                )
+        if verbose:
+            print(
+                f"{datetime.now().strftime('%H:%M:%S')}|{dataset}|{r_coef} ({i+1}/{len(regularization_coefficients)})"
+            )
+        model.fit(x_train, y_train)
+        train_predictions = model.predict(x_train)
+        test_predictions = model.predict(x_test)
+        train_accuracy = accuracy_score(y_train, train_predictions)
+        test_accuracy = accuracy_score(y_test, test_predictions)
+        accuracies[i] = train_accuracy, test_accuracy
+    regularization_coefficients = regularization_coefficients.reshape(-1, 1)
+    return np.hstack((regularization_coefficients, accuracies))
+
+
+if __name__ == "__main__":
+    reg_coefs = np.array([2**x for x in range(-10, 11)])
+    ret = regularization_experiment("penguins", "linear", reg_coefs, verbose=True)
+    print(ret)
